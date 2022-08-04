fd111d892708541b5f50d9015aba4b1ea63969f6
Julia Babina <u.babina55@gmail.com>
2022-08-04 23:30:38 +0500

    add sqrt

--- S1/Program.cs
+++ S1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
+// number *= number
+// sqrt = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("Число в квадрате: " + sqrt);
+
