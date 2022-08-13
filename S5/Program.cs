ad3bc6b408baf559b54f222068eeb142abfba8a5
Julia Babina <u.babina55@gmail.com>
2022-08-13 18:38:25 +0500

    add t22 to S5

--- S5/Program.cs
+++ S5/Program.cs
@@ -238,17 +238,40 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
+// Console.Clear();
+// Console.Write("Введите X1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2=int.Parse(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
+
+// Console.WriteLine($"d={d:f2}");
+
+
+
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
 Console.Clear();
-Console.Write("Введите X1: ");
-int x1=int.Parse(Console.ReadLine());
-Console.Write("Введите Y1: ");
-int y1=int.Parse(Console.ReadLine());
-Console.Write("Введите X2: ");
-int x2=int.Parse(Console.ReadLine());
-Console.Write("Введите Y2: ");
-int y2=int.Parse(Console.ReadLine());
 
-double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
+Console.WriteLine("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double sqrt = 1;
+int number = 1;
 
-Console.WriteLine($"d={d:f2}");
+Console.Write($"These are squares of numbers from 1 to {n}: ");
+
+while (number <= n)
+{
+    sqrt = Math.Pow(number, 2);
+    Console.Write(sqrt + ", ");
+    number++;
+}
+
 
